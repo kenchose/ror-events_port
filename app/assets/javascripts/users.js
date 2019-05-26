63d2d289d8b8b077b747cee02048adbb933bd11a
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2019-05-26 01:39:36 -0700

    finalize event-edit view page fixed footer and form route

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function(){
+$(document).on('turbolinks:load', () => {
     $("input[name=terms]").change(function(){
         $("#signup").removeClass("ui disabled yellow button large").addClass("ui yellow button");
     });
@@ -27,6 +27,5 @@ $(document).ready(function(){
     $('.red.cancel.button').on('click', function(){
         $('.small.test.modal').modal("hide");
     });
-
 });
 
